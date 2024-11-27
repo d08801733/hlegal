fadc519aa716996a98a81d7128252792713c3dd1
Dmytro <d08801733@gmail.com>
2024-11-27 23:44:45 +0200

    Refactor team__wrapper

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,5 @@
-$(document).ready(function () {
-  $(".team__wrapper").slick({
-    dots: true,
-  });
-});
+// $(document).ready(function () {
+//   $(".team__wrapper").slick({
+//     dots: true,
+//   });
+// });
