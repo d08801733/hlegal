b0fcde2bb51f0d75ff91b5671ec623d757d10971
Dmytro <d08801733@gmail.com>
2024-12-03 16:23:50 +0200

    Modify gulpfile.js. Change gulp-minify. Add updateHTMLPaths and updateCSSPaths tasks

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,9 @@ const autoprefixer = require("gulp-autoprefixer");
 const rename = require("gulp-rename");
 const minify = require("gulp-minify");
 const webp = require("gulp-webp");
+const replace = require("gulp-replace");
+const path = require("path");
+const log = require("fancy-log");
 
 gulp.task("server", function () {
   browserSync({
@@ -30,26 +33,49 @@ gulp.task("styles", function () {
 
 gulp.task("scripts", function () {
   gulp
-    .src(["src/js/**/*.js", "src/js/**/*.mjs"])
+    .src(["src/js/**/script.js", "src/js/**/hamburger.js"])
     .pipe(minify())
     .pipe(gulp.dest("src/js"));
 });
 
 gulp.task("convertToWebp", function () {
-  gulp.src(
-    "images/**/*.{jpg, jpeg, png}",
-    { encoding: false }.pipe(webp()).pipe(gulp.dest("src/optimages"))
-  );
+  gulp
+    .src("src/images/**/*.{jpg,jpeg,png}", { encoding: false }) // Берем все изображения из исходной папки
+    .pipe(webp()) // Конвертируем в формат webp
+    .pipe(gulp.dest("src/optimages")); // Сохраняем в папку назначения
+});
+
+gulp.task("updateHTMLPaths", function () {
+  gulp
+    .src(["src/*.html"]) // Берем HTML файлы
+    .pipe(replace("images", "optimages"))
+    .pipe(gulp.dest("src")); // Сохраняем измененные файлы
+});
+
+gulp.task("updateCSSPaths", function () {
+  gulp
+    .src(["src/css/*.css"]) // Берем CSS файлы
+    .pipe(replace("images", "optimages"))
+    .pipe(gulp.dest("src/css")); // Сохраняем измененные файлы
 });
 
 gulp.task("watch", function () {
   gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel("styles"));
   gulp.watch("src/js/**/*.+(js|mjs)"), gulp.parallel("scripts");
-  gulp.watch("src/images/**/*.{jpg, jpeg, png}"),
-    gulp.parallel("convertToWebp");
+  gulp.watch("src/images/**/*.{jpg,jpeg,png}"), gulp.parallel("convertToWebp");
+  gulp.watch("src/**/*.html"), gulp.parallel("updateHTMLPaths");
+  gulp.watch("src/**/*.css"), gulp.parallel("updateCSSPaths");
 });
 
 gulp.task(
   "default",
-  gulp.parallel("watch", "server", "styles", "scripts", "convertToWebp")
+  gulp.parallel(
+    "watch",
+    "server",
+    "styles",
+    "scripts",
+    "convertToWebp",
+    "updateHTMLPaths",
+    "updateCSSPaths"
+  )
 );
